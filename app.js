9542d0c2f4c7212d7698d1bfcbd3aa071659fb61
Bagus Tri Prasetyo <bagus.tri@mediatropy.com>
2024-02-13 02:02:38 +0700

    need to add more configuration for Database Table Relations, still thinking about it

---
+++ app.js
@@ -0,0 +1,7 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use()
---
+++ test.js
@@ -0,0 +1,4 @@
+// console.log(atob('lorem ipsum dolor sit amet')) // Return => Þ*lºghìÖ¦z  ||
+//                                                                          || Useless simple method to hash secretcode
+// console.log(btoa('Þ*lºghìÖ¦z')) // Return =>3ipsumdo7Nameg==             ||
+
